19b23be52aaf7a4f49e83e201c2ae92a462e151c
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-03-17 23:11:27 +0100

    Add decorator class

---
+++ HelloWorld/Decorator/Decorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld.Decorator {
+    class Decorator : ICountry {
+        private ICountry _country;
+
+        public Decorator(ICountry country) => this.Country = country;
+
+        internal ICountry Country { get => _country; set => _country = value; }
+
+        public string getDate() {
+            throw new NotImplementedException();
+        }
+
+        public string getHello() {
+            throw new NotImplementedException();
+        }
+
+        public string getHelloMessage() {
+            throw new NotImplementedException();
+        }
+    }
+}
