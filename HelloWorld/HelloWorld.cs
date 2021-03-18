d932e48798265c1f0e420c980f6b4bed390c45df
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-03-18 13:50:40 +0100

    add space in main

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -7,12 +7,16 @@ namespace HelloWorld {
         static void Main() {
             ICountry france = new Smiley(new France());
             Console.WriteLine(france.getHelloMessage());
+
             ICountry unitedKingdom = new Border(new UnitedKingdom());
             Console.WriteLine(unitedKingdom.getHelloMessage());
+
             ICountry unitedStatesOfAmerica = new BorderStar(new Smiley(new UnitedStatesOfAmerica()));
             Console.WriteLine(unitedStatesOfAmerica.getHelloMessage());
+
             ICountry indonesia = new BorderUnderscore(new BorderStar(new Smiley(new Smiley(new Indonesia()))));
             Console.WriteLine(indonesia.getHelloMessage());
+
             Console.ReadLine();
         }
     }
