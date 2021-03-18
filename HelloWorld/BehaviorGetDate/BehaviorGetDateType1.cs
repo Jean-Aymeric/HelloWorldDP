13f4b008b406b589db19f23645e41deba7665652
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-03-18 13:50:14 +0100

    add space in main

--- HelloWorld/BehaviorGetDate/BehaviorGetDateType1.cs
+++ HelloWorld/BehaviorGetDate/BehaviorGetDateType1.cs
@@ -3,7 +3,7 @@
 namespace HelloWorld.BehaviorGetDate {
     class BehaviorGetDateType1 : IBehaviorGetDate {
         public string getDate() {
-            return DateTime.Now.ToString("MM/dd/yyyy");
+            return DateTime.Now.ToString("dd/MM/yyyy");
         }
     }
 }
--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,12 +6,16 @@ namespace HelloWorld {
         static void Main() {
             ICountry france = new France();
             Console.WriteLine(france.getHelloMessage());
+
             ICountry unitedKingdom = new UnitedKingdom();
             Console.WriteLine(unitedKingdom.getHelloMessage());
+
             ICountry unitedStatesOfAmerica = new UnitedStatesOfAmerica();
             Console.WriteLine(unitedStatesOfAmerica.getHelloMessage());
+
             ICountry indonesia = new Indonesia();
             Console.WriteLine(indonesia.getHelloMessage());
+
             Console.ReadLine();
         }
     }
