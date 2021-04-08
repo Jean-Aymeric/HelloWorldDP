1aa735721bdafbb4371ffb947173a2d22bd806c9
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-04-08 10:22:54 +0200

    Prototype done

--- HelloWorld/HelloWorld/AbstractFactory/FactorySimple.cs
+++ HelloWorld/HelloWorld/AbstractFactory/FactorySimple.cs
@@ -4,11 +4,11 @@ using HelloWorld.Countries;
 namespace HelloWorld.AbstractFactory {
     class FactorySimple : AbstractFactoryHelloWorld {
         public override ICountry makeFrance() {
-            return (Country) new BuilderFrenchType1().getCountry();
+            return new BuilderFrenchType1().getCountry();
         }
 
         public override ICountry makeIndonesia() {
-            return (Country)new BuilderIndonesianType2().getCountry();
+            return new BuilderIndonesianType2().getCountry();
         }
 
         public override ICountry makeLatin() {
@@ -16,11 +16,11 @@ namespace HelloWorld.AbstractFactory {
         }
 
         public override ICountry makeUnitedKingdom() {
-            return (Country)new BuilderUnitedKingdomType1().getCountry();
+            return new BuilderUnitedKingdomType1().getCountry();
         }
 
         public override ICountry makeUnitedStatesOfAmerica() {
-            return (Country)new BuilderUnitedStatesOfAmericaType2().getCountry();
+            return new BuilderUnitedStatesOfAmericaType2().getCountry();
         }
     }
 }
