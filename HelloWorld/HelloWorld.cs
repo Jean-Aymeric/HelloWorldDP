aaa9cd27832e571599970a2721d90faffc2edd9e
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-04-19 21:51:50 +0200

    flyweight done

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -5,6 +5,7 @@ namespace Main {
     abstract class HelloWorld {
         static void Main() {
             FacadeHelloWorld facadeHelloWorld = FacadeHelloWorld.getInstance();
+
             Console.WriteLine(facadeHelloWorld.makeByParse("FR :) :) - *").getHelloMessage());
             foreach (string countryName in facadeHelloWorld.getFactoryNames()) {
                 Console.WriteLine(facadeHelloWorld.make(countryName).getHelloMessage());
